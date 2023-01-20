20ca0241c54fa2b41383ad5ad3cae3441eb5652c
Sean Conrad <seanconrad123@gmail.com>
2023-01-20 17:42:15 -0500

    added some video urls to the data

---
+++ data/videoURLs.ts
@@ -0,0 +1,7 @@
+export const videoURLS = [
+	'https://www.youtube.com/watch?v=rBLYgDvVIS4',
+	'https://www.youtube.com/watch?v=13bwZ0_5IVg',
+	'https://www.youtube.com/shorts/3TEtsbynx6g',
+	'https://www.youtube.com/shorts/F6guu6jX7rI',
+
+]
