447a81d8d71a68cbaf4182963e6799727456f483
Sean Conrad <seanconrad123@gmail.com>
2023-02-03 19:55:19 -0500

    fixed bg color

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,46 +18,48 @@ export default function Home() {
 	})
 
   return (
-    <div className='container mx-auto'>
-      <Head>
-        <title>Skater Lorem Ipsum Generator</title>
-        <meta name="description" content="Skater lorem ipsum generator" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+		<div className='bg-[#282a36]'>
+			<div className='container mx-auto '>
+				<Head>
+					<title>Skater Lorem Ipsum Generator</title>
+					<meta name="description" content="Skater lorem ipsum generator" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+				
 			
-		
-      <main>
-        <h1 className='text-7xl text-center text-white'>
-          Skater Lorem Ipsum Generator
-        </h1>
+				<main>
+					<h1 className='text-7xl text-center text-white'>
+						Skater Lorem Ipsum Generator
+					</h1>
 
-				<div className='text-white'>{paragraphCount} {paragraphCount <= 1 ? 'paragraph' : 'paragraphs'}</div>
-				<input 
-					type="range" 
-					min="1" 
-					max="50" 
-					onChange={e => setParagraphCount(Number(e.target.value))} 
-					value={paragraphCount} 
-					className="w-64" 
-					>
-				</input>
+					<div className='text-white'>{paragraphCount} {paragraphCount <= 1 ? 'paragraph' : 'paragraphs'}</div>
+					<input 
+						type="range" 
+						min="1" 
+						max="50" 
+						onChange={e => setParagraphCount(Number(e.target.value))} 
+						value={paragraphCount} 
+						className="w-64" 
+						>
+					</input>
 
-				<div className='text-white'>{wordCount} {wordCount <= 1 ? 'word' : 'words'}</div>
-				<input 
-					type="range" 
-					min="1" 
-					max={ipsumWords.length}
-					onChange={e => setWordCount(Number(e.target.value))} 
-					value={wordCount} 
-					className="w-64" 
-					>
-				</input>
+					<div className='text-white'>{wordCount} {wordCount <= 1 ? 'word' : 'words'}</div>
+					<input 
+						type="range" 
+						min="1" 
+						max={ipsumWords.length}
+						onChange={e => setWordCount(Number(e.target.value))} 
+						value={wordCount} 
+						className="w-64" 
+						>
+					</input>
 
-				<div className='grid grid-cols-2 gap-4'>
-					{generate}
-				</div>
+					<div className='grid grid-cols-2 gap-4'>
+						{generate}
+					</div>
 
-      </main>
-    </div>
+				</main>
+			</div>
+		</div>
   )
 }
